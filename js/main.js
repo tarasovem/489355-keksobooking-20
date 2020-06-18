243dca6f6d36fe1d18555c6b4aac0481ec92c4a6
Evgeniy Tarasov <tarasovem@gmail.com>
2020-06-19 00:19:16 +0300

    Добавляет кастомную валидацию соотношения комнат и гостей

--- js/main.js
+++ js/main.js
@@ -201,15 +201,15 @@ var getElementCoord = function (elem) {
   return {
     top: elem.offsetTop,
     left: elem.offsetLeft
-  }
-}
+  };
+};
 
 var mainMapPin = document.querySelector('.map__pin--main');
 
 var setAddress = function (x, y) {
   var addressInput = document.querySelector('#address');
 
-  addressInput.value = x + ', ' + y
+  addressInput.value = x + ', ' + y;
 };
 
 var documentLoadHandler = function () {
@@ -241,10 +241,39 @@ var enableElements = function () {
   }
 };
 
+var roomsNumberSelect = document.querySelector('#room_number');
+var capacitySelect = document.querySelector('#capacity');
+
+var setCapacityWarningMessage = function () {
+  var roomsNumberSelectValue = roomsNumberSelect.value;
+  var capacitySelectValue = capacitySelect.value;
+
+  if (roomsNumberSelectValue === '100' && capacitySelectValue !== '0') {
+    capacitySelect.setCustomValidity('Гостей приглашать нельзя!');
+  } else if (roomsNumberSelectValue < capacitySelectValue) {
+    capacitySelect.setCustomValidity('Количество комнат мало для такого количества гостей!');
+  } else {
+    capacitySelect.setCustomValidity('');
+  }
+};
+
+var roomsNumberSelectInputHandler = function () {
+  setCapacityWarningMessage();
+};
+
+capacitySelect.addEventListener('input', roomsNumberSelectInputHandler);
+
+var capacitySelectInputHandler = function () {
+  setCapacityWarningMessage();
+};
+
+roomsNumberSelect.addEventListener('input', capacitySelectInputHandler);
+
 var mainMapPinMousedownHandler = function (evt) {
   if (evt.button === 0) {
     enableElements();
     setAddress(getElementCoord(mainMapPin).top + MAIN_MAP_PIN.enabled.width / 2, getElementCoord(mainMapPin).left + MAIN_MAP_PIN.enabled.height);
+    setCapacityWarningMessage();
   }
 };
 
@@ -254,88 +283,8 @@ var mainMapPinKeydownHandler = function (evt) {
   if (evt.key === 'Enter') {
     enableElements();
     setAddress(getElementCoord(mainMapPin).top + MAIN_MAP_PIN.enabled.width / 2, getElementCoord(mainMapPin).left + MAIN_MAP_PIN.enabled.height);
+    setCapacityWarningMessage();
   }
 };
 
-mainMapPin.addEventListener('keydown', mainMapPinKeydownHandler)
-
-
-
-
-
-/*
-
-
-var setAddress = function (coordX, coordY) {
-  var addressInput = document.querySelector('#address');
-
-  addressInput.value = coordX + ', ' + coordY;
-};
-
-var mainMapPin = document.querySelector('.map__pin--main img');
-
-var getCoords = function (elem) {
-  let box = elem.getBoundingClientRect();
-
-  return {
-    top: Math.round(box.top + pageYOffset),
-    left: Math.round(box.left + pageXOffset),
-  }
-};
-
-var MAIN_PIN = {
-  disabled: {
-    width: 62,
-    height: 62,
-  },
-  enabled: {
-    width: 62,
-    height: 84,
-  },
-};
-
-var documentLoadHandler = function () {
-  setDisableState();
-
-  var mainPinCoords = getCoords(mainMapPin);
-  setAddress(mainPinCoords.left + MAIN_PIN.disabled.width / 2, mainPinCoords.top + MAIN_PIN.disabled.height / 2);
-};
-
-var mainMapPinMousedownHandler = function (evt) {
-  if (evt.button === 0) {
-    console.log(evt.target);
-    var mainPinCoords = getCoords(evt.target);
-    setAddress(mainPinCoords.left + MAIN_PIN.enabled.width / 2, mainPinCoords.top + MAIN_PIN.enabled.height);
-
-    removeDisableState();
-
-    var pinsMap = document.querySelector('.map__pins');
-
-    var pinsM
-  }
-};
-
-document.addEventListener('DOMContentLoaded', documentLoadHandler);
-
-mainMapPin.addEventListener('mousedown', mainMapPinMousedownHandler);
-
-mainMapPin.addEventListener('keydown', function (evt) {
-  if (evt.key === 'Enter') {
-    removeDisableState();
-  }
-});
-
-
-
-var mapPins = document.querySelector('.map__pins');
-
-
-
-
-var getAddress = function (evt) {
-  var target = evt.target;
-
-  console.log(getCoords(target));
-}
-*/
-
+mainMapPin.addEventListener('keydown', mainMapPinKeydownHandler);
